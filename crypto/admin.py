77347cf19fc848487db30ada7336a95d4b6d63c8
Vladimir <razdrogin.v.a@edu.mirea.ru>
2018-12-18 02:10:18 +0300

    add models to admin

--- crypto/admin.py
+++ crypto/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
-from .models import Orders
-
+from .models import Orders, Topic, Message
 
 
 class OrdersAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'date', 'price', 'quantity')
 
 
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'id_razdel', 'name_theme', 'name_creator', 'name_last_answer', 'date_last_answer')
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'id_topic', 'text_mes', 'name_man', 'date_answer')
+
+
 admin.site.register(Orders, OrdersAdmin)
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Message, MessageAdmin)
